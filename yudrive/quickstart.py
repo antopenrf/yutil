4bab5754306cab3bc2a8df98f98cec4de0e8d333
antopenrf <antopenrf@gmail.com>
2015-04-20 23:06:04 +0800

    add yudrive quickstart py

---
+++ yudrive/quickstart.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+from pydrive.auth import GoogleAuth
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+
+
+
+from pydrive.drive import GoogleDrive
+
+drive = GoogleDrive(gauth)
+
+file1 = drive.CreateFile({'title': 'Hello.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'
+file1.SetContentString('Hello World!') # Set content of the file from given string
+file1.Upload()
+
+
+# Auto-iterate through all files that matches this query
+file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+for file1 in file_list:
+  print 'title: %s, id: %s' % (file1['title'], file1['id'])
