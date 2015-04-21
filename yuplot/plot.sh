8043d4fa6b4e35436bf93f19376f5f78296270a8
antopenrf <antopenrf@gmail.com>
2015-04-22 00:18:25 +0800

    added lables and logscales

--- yuplot/plot.sh
+++ yuplot/plot.sh
@@ -1,1 +1,1 @@
-gnuplot -e "plot 'data.txt' using 1:2 title '2' with lines,plot 'data.txt' using 1:3 title '3' with lines,plot 'data.txt' using 1:4 title '4' with lines; set term png; set output 'plot.png'; replot"
+gnuplot -e " plot 'data.txt' using 1:2 title '1st' with lines, 'data.txt' using 1:3 title '2nd' with lines, 'data.txt' using 1:4 title '3rd' with lines, 'data.txt' using 1:5 title '4th' with lines; set term png;set xlabel 'x_axis';set ylabel 'y_axis';set logscale x; set output 'plot.png'; replot"
--- yuplot/yuplot.py
+++ yuplot/yuplot.py
@@ -9,42 +9,91 @@ import os
 
 class yuplot(object):
 
-    def _wrt_plot_shell(self, content):
+    def _xlog_on(self):
+        if self.xlog == '0':
+            return ''
+        else:
+            return "set logscale x;"
+
+    def _ylog_on(self):
+        if self.ylog == '0':
+            return ''
+        else:
+            return "set logscale y;"
+
+    def _add_xlabel(self):
+        return "set xlabel '" + self.xlabel + "';"
+
+    def _add_ylabel(self):
+        return "set ylabel '" + self.ylabel + "';"
+
+    def _wrt_plot_shell(self):
+        self.plot_string = "gnuplot -e \" plot" + self._add_data() + ";" + " set term png;" + self._add_xlabel() + self._add_ylabel() + self._xlog_on() + self._ylog_on() + " set output 'plot.png'; replot\"\n"
+
         with open(self.shfile, 'w') as f:
-            f.write(content)
+            f.write(self.plot_string)
 
 
     def _add_data(self, ref = 1):
         strings_to_return = ''
-        for n in range(self.columns_to_plot-1):
-            strings_to_return += "plot '" + self.outputfile + "' using " + str(ref) + ":" + str(n + 2) + " title " + "'" + str(n + 2) + "'" + " with lines"
+        for n in range(self.columns_to_plot):
+            strings_to_return += " '" + self.outputfile + "' using " + str(ref) + ":" + str(n + 2) + " title " + "'" + str(self.legends[n+1]) + "'" + " with lines"
             if n != self.columns_to_plot - 1:
                 strings_to_return += ','
         return strings_to_return
 
     def __init__(self, inputfile = None, outputfile = 'data.txt', shfile = 'plot.sh'):
         
-        if len(sys.argv) != 2:
-            print("Usage: python yuplot.py inputfile/n")
+        if len(sys.argv) == 1:
+            print("Usage: python yuplot.py inputfile xlable ylabel [0 or 1 for x logscable] [0 or 1 for y logscable]/n")
 
         else:
+            self.plot_string = None
             self.inputfile = sys.argv[1]
             self.outputfile = outputfile
             self.shfile = shfile
-            
+            try:
+                self.xlabel = sys.argv[2]
+            except:
+                self.xlabel = 'x'
+
+            try:
+                self.ylabel = sys.argv[3]
+            except:
+                self.ylabel = 'y'
+
+            try:
+                self.xlog = sys.argv[4]
+            except:
+                self.xlog = '0'
+
+            try:
+                self.ylog = sys.argv[5]
+            except:
+                self.ylog = '0'
+                
             with open(self.inputfile, 'r') as f:
                 all_lines = f.readlines()
                 
             with open(self.outputfile, 'w') as f:
-                for each in all_lines:
+                for count,each in enumerate(all_lines):
+                    if count == 0:
+                        each = each.replace("\r","")
+                        each = each.replace(",", "\t")
+                        self.columns_to_plot = len(each.split()) - 1
+                        print 'a',self.columns_to_plot
+                        try:
+                            float(each[0])
+                            self.legends = range(self.columns_to_plot+1)
+                        except ValueError:
+                            each = each.replace("\r","")
+                            each = each.replace(",", "\t")
+                            self.legends = each.split()
                     each = each.replace("\r","")
                     each = each.replace(",", "\t")
                     f.write(each)
-            self.columns_to_plot = len(each.split()) - 1
-            print self.columns_to_plot
-            self.plot_string = "gnuplot -e \"" + self._add_data() + ";" + " set term png; set output 'plot.png'; replot\"\n"
 
-            self._wrt_plot_shell(self.plot_string)
+            self._wrt_plot_shell()
             
 
 if __name__ == '__main__':
