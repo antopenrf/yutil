6558d40b8df58eeb39529d29eb68c80a26d32be6
yulung <yulung@gmail.com>
2015-05-01 21:58:44 +0800

    commit

--- yuplot/plot.sh
+++ yuplot/plot.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-gnuplot -e " plot 'data.txt' using 1:2 title 'gain' with lines; set term png;set xlabel 'freq_GHz';set ylabel 'gain_dB'; set output 'plot.png';set title '3126_dipole'; set grid; replot"
-=======
-gnuplot -e " plot 'data.txt' using 1:2 title '1' with lines; set term png;set xlabel 'freq (GHz)';set ylabel 'gain (dB)'; set output 'plot.png'; replot"
->>>>>>> 1b9a8d35607f7e2a15fe1409c2561e0d48c9f05d
+gnuplot -e " plot 'data.txt' using 1:2 title 'gain' with lines; set term png;set xlabel 'x';set ylabel 'y'; set output 'plot.png';set title 'plot'; set grid; replot"
