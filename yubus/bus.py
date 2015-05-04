72cd2e9061a399296305c5bb1b8cc40a73671388
antopenrf <antopenrf@gmail.com>
2015-05-04 21:49:54 +0800

    commit

--- yubus/bus.py
+++ yubus/bus.py
@@ -1,5 +1,6 @@
 import urllib
 import sys
+import re
 
 if len(sys.argv) < 2:
     bus = 285
@@ -15,7 +16,17 @@ data = data.split()
 
 
 
-for i,each in enumerate(data):
+for i,each in enumerate(data[:-10]):
+    each = re.sub(r'<.*>*', '', each)
+    each = re.sub(r'.*>$', '', each)
+    each = re.sub(r'href.*>', '', each)
+    each = re.sub(r'align.*', '', each)
+    each = re.sub(r'nowrap>', '', each)
+    each = re.sub(r'border=0', '', each)
+    each = re.sub(r'style=', '', each)
+    each = re.sub(r'"color.*', '', each)
+    each = re.sub(r'"810.*', '', each)    
+    each = re.sub(r'title=', '', each)            
     print(each)
 
             
