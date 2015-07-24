410dccba7212d851539f2b863923f912b3d5c7bd
yulung <yulung@gmail.com>
2015-07-24 14:50:37 +0800

    commit

--- yuplot/plot.sh
+++ yuplot/plot.sh
@@ -1,1 +1,1 @@
-gnuplot -e " plot 'data.txt' using 1:2 title 'gain' with lines; set term png;set xlabel 'x';set ylabel 'y'; set output 'plot.png';set title 'plot'; set grid; replot"
+gnuplot -e " plot 'data.txt' using 1:2 title '1' with lines; set term png;set xlabel 'x';set ylabel 'y'; set output 'plot.png';set title 'plot'; set grid; replot"
